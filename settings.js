07eddafff731906369ae61c25da5910b772e1604
tmg054 <govhatadiwa@gmail.com>
2025-05-02 13:13:10 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Tadiwagovh', // Your name
-  ownerNumber: '263714101261', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Tadiwagovha', // Your name
+  ownerNumber: '263789304671', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
